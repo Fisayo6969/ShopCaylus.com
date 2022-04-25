b05df53c82a792fd59140e3ccd58960d5455efc0
oluwafisayomi <wrld26993@gmail.com>
2022-04-25 17:14:03 +0100

    mailing list

---
+++ script.js
@@ -0,0 +1,249 @@
+function displayimg(){
+    var z = document.getElementById("item2");
+    var y = document.getElementById("item1");
+    if(y.type === 'item1'){
+       y.style.display = "block";
+       z.style.display = "none";
+    }
+    else{
+       y.style.display = "none";
+       z.style.display = "block";
+    }
+}
+function displayimage(){
+    var y = document.getElementById("item1");
+    var z = document.getElementById("item2");
+    if(z.type === 'item2'){
+       y.style.display = "none";
+       z.style.display = "block";
+    }
+    else{
+       y.style.display = "block";
+       z.style.display = "none";
+    }
+}
+// 
+function displayimg3(){
+    var z = document.getElementById("item4");
+    var y = document.getElementById("item3");
+    if(y.type === 'item3'){
+       y.style.display = "block";
+       z.style.display = "none";
+    }
+    else{
+       y.style.display = "none";
+       z.style.display = "block";
+    }
+}
+function displayimage4(){
+    var y = document.getElementById("item3");
+    var z = document.getElementById("item4");
+    if(z.type === 'item4'){
+       y.style.display = "none";
+       z.style.display = "block";
+    }
+    else{
+       y.style.display = "block";
+       z.style.display = "none";
+    }
+}
+// 
+function displayimg5(){
+    var z = document.getElementById("item6");
+    var y = document.getElementById("item5");
+    if(y.type === 'item5'){
+       y.style.display = "block";
+       z.style.display = "none";
+    }
+    else{
+       y.style.display = "none";
+       z.style.display = "block";
+    }
+}
+function displayimage6(){
+    var y = document.getElementById("item5");
+    var z = document.getElementById("item6");
+    if(z.type === 'item5'){
+       y.style.display = "none";
+       z.style.display = "block";
+    }
+    else{
+       y.style.display = "block";
+       z.style.display = "none";
+    }
+}
+// 
+function displayimg7(){
+    var z = document.getElementById("item8");
+    var y = document.getElementById("item7");
+    if(y.type === 'item7'){
+       y.style.display = "block";
+       z.style.display = "none";
+    }
+    else{
+       y.style.display = "none";
+       z.style.display = "block";
+    }
+}
+function displayimage8(){
+    var y = document.getElementById("item7");
+    var z = document.getElementById("item8");
+    if(z.type === 'item7'){
+       y.style.display = "none";
+       z.style.display = "block";
+    }
+    else{
+       y.style.display = "block";
+       z.style.display = "none";
+    }
+}
+// 
+function img1(){
+   var z = document.getElementById("img2");
+   var y = document.getElementById("img1");
+   if(y.type === 'img1'){
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+   else{
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+}
+function img2(){
+   var y = document.getElementById("img1");
+   var z = document.getElementById("img2");
+   if(z.type === 'img1'){
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+   else{
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+}
+// 
+function img3(){
+   var z = document.getElementById("img4");
+   var y = document.getElementById("img3");
+   if(y.type === 'img3'){
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+   else{
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+}
+function img4(){
+   var y = document.getElementById("img3");
+   var z = document.getElementById("img4");
+   if(z.type === 'img3'){
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+   else{
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+}
+// 
+function img5(){
+   var z = document.getElementById("img6");
+   var y = document.getElementById("img5");
+   if(y.type === 'img5'){
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+   else{
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+}
+function img6(){
+   var y = document.getElementById("img5");
+   var z = document.getElementById("img6");
+   if(z.type === 'img5'){
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+   else{
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+}
+// 
+function img7(){
+   var z = document.getElementById("img8");
+   var y = document.getElementById("img7");
+   if(y.type === 'img7'){
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+   else{
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+}
+function img8(){
+   var y = document.getElementById("img7");
+   var z = document.getElementById("img8");
+   if(z.type === 'img7'){
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+   else{
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+}
+// 
+function img9(){
+   var z = document.getElementById("img10");
+   var y = document.getElementById("img9");
+   if(y.type === 'img9'){
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+   else{
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+}
+function img10(){
+   var y = document.getElementById("img9");
+   var z = document.getElementById("img10");
+   if(z.type === 'img9'){
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+   else{
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+}
+// 
+function img11(){
+   var z = document.getElementById("img12");
+   var y = document.getElementById("img11");
+   if(y.type === 'img11'){
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+   else{
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+}
+function img12(){
+   var y = document.getElementById("img11");
+   var z = document.getElementById("img12");
+   if(z.type === 'img11'){
+      y.style.display = "none";
+      z.style.display = "block";
+   }
+   else{
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+}
